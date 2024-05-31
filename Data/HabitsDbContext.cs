25d57e8211983306fb70ce0436b708362a9b9c54
Flynn Bui <kuboy26@gmail.com>
2024-05-31 11:42:26 +0700

    update user entity and dbcontext

--- Data/HabitsDbContext.cs
+++ Data/HabitsDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using backend.Habits.Entities;
+using backend.Users.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace backend.Data;
@@ -9,5 +10,19 @@ public class HabitsDbContext : DbContext
     {
     }
     public DbSet<Habit> Habits { get; set; }
+    public DbSet<User> Users {get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>()
+            .HasMany(u => u.Habits)
+            .WithOne(h => h.User)
+            .HasForeignKey(h => h.UserId);
+        
+        modelBuilder.Entity<User>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+                
+        base.OnModelCreating(modelBuilder);
+    }
 
 }
